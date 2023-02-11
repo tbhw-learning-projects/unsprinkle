d6a39ab1b7cbf5b59076e2cf33f0347b0953d10e
Tanner B. Hess Webber <tb@hesswebber.com>
2023-02-11 13:48:39 +0000

    fix(tags): makes tags render in a single row

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -50,17 +50,24 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  padding: 8px 0;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 `;
 
 const Tag = styled.li`
+  display: inline;
+
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+
+  &:not(:last-child) {
+    margin-right: 8px;
+  }
 `;
 
 export default PhotoGridItem;
